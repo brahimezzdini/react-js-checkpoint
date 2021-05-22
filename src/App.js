59d78990047fa1049d5bbebe455bf151352b18b9
brahim ezz <ezzdinib@gmail.com>
2021-05-22 12:36:28 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Form from "./Components/form/Form";
+
+
+function App() {
+  return (
+    <div>
+     <Form></Form>
+    </div>
+    
+      );
+}
+
+export default App;
